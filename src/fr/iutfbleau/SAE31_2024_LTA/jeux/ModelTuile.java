9f4133a5abc040e71164f0fddc6ec3c638fba6a4
Louis TANCHOU <tanchou@noreply.grond.iut-fbleau.fr>
2024-10-20 21:36:55 +0200

    Actualiser src/fr/iutfbleau/SAE31_2024_LTA/jeux/ModelTuile.java
    Point virgule

--- src/fr/iutfbleau/SAE31_2024_LTA/jeux/ModelTuile.java
+++ src/fr/iutfbleau/SAE31_2024_LTA/jeux/ModelTuile.java
@@ -32,11 +32,11 @@ public class ModelTuile {
         indexBiome[4] = 4; //Foret
 
         int indexCouleur1 = indexBiome[random.nextInt(indexBiome.length)];
-        int indexCouleur2 = indexBiome[random.nextInt(indexBiome.length)]
+        int indexCouleur2 = indexBiome[random.nextInt(indexBiome.length)];
         if (indexCouleur1 == indexCouleur2){
           random = new Random();
-          random.setSeed(seed+1)
-           indexCouleur2 = indexBiome[random.nextInt(indexBiome.length)];
+          random.setSeed(seed+1);
+          indexCouleur2 = indexBiome[random.nextInt(indexBiome.length)];
         }
 
         int territory = random.nextInt(composition.length+1);
