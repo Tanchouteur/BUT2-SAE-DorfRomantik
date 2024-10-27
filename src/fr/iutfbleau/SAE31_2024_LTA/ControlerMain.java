ae09fc6ba7552d2e3d2237eab38856bac25a4591
renaultth <thomas.renault@etu.u-pec.fr>
2024-10-27 20:24:15 +0100

    update thomas : ajout de la javadoc pour le ControlerMain

--- src/fr/iutfbleau/SAE31_2024_LTA/ControlerMain.java
+++ src/fr/iutfbleau/SAE31_2024_LTA/ControlerMain.java
@@ -5,7 +5,8 @@ import fr.iutfbleau.SAE31_2024_LTA.loading.ModelLoader;
 import fr.iutfbleau.SAE31_2024_LTA.loading.WindowStateHandler;
 
 /**
- * Classe principale qui s'occupe
+ * Classe principale qui gère le lancement de l'application.
+ * elle s'occupe de l'initialisation du modèle principal de l'écran de chargement.
  */
 public class ControlerMain {
 
@@ -28,18 +29,39 @@ public class ControlerMain {
         modelPrincipale.getVuePrincipale().addWindowListener(new WindowStateHandler());
     }
 
+    /**
+     * méthode qui définit le modèle principal.
+     *
+     * @param model l'instance du modèle principal à définir.
+     */
     public static synchronized void setModelPrincipale(ModelPrincipale model) {
         modelPrincipale = model;
     }
 
+    /**
+     * méthode qui renvoie l'instance du modèle principal.
+     *
+     * @return l'instance du modèle principal.
+     */
     public static synchronized ModelPrincipale getModelPrincipale() {
         return modelPrincipale;
     }
 
+
+    /**
+     * méthode qui définit l'état de chargement.
+     *
+     * @param loading vrai si le chargement est en cours, faux sinon.
+     */
     public static synchronized void setIsLoading(boolean loading) {
         isLoading = loading;
     }
 
+    /**
+     * méthode qui vérifie si l'application est en cours de chargement.
+     *
+     * @return vrai si le chargement est en cours, faux sinon.
+     */
     public static synchronized boolean isIsLoading() {
         return isLoading;
     }
